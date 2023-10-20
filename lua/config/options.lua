b9d3631ef1331373f17b3036048ead058325da35
Emil Privér <emil@priver.dev>
2023-10-20 20:29:08 +0200

    fixed config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,4 +4,13 @@
 --
 vim.g.everforest_background = "soft"
 
-vim.lsp.buf.inlay_hint(0)
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.server_capabilities.inlayHintProvider then
+      vim.lsp.inlay_hint(args.buf, true)
+    end
+    -- whatever other lsp config you want
+  end,
+})
