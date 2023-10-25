7172d819ba83bc85bfec9e2013851b26112b11f9
Emil Privér <emil@priver.dev>
2023-10-25 23:52:12 +0200

    dev

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- This file is automatically loaded by lazyvim.config.initial
+--
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,14 +3,3 @@
 -- Add any additional options here
 --
 vim.g.everforest_background = "soft"
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client.server_capabilities.inlayHintProvider then
-      vim.lsp.inlay_hint(args.buf, true)
-    end
-    -- whatever other lsp config you want
-  end,
-})
