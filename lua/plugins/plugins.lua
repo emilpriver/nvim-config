1a24ecd3814381f5c82192c982de6cc94ad92f9e
emilpriver <emil@priver.dev>
2024-07-29 10:36:53 +0200

    add elixir support

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -143,6 +143,9 @@ return {
         "yaml",
         "rust",
         "go",
+        "elixir",
+        "eex",
+        "heex",
       },
     },
   },
@@ -368,6 +371,7 @@ return {
   "othree/html5.vim",
   "pangloss/vim-javascript",
   "evanleck/vim-svelte",
+
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
@@ -375,5 +379,37 @@ return {
     build = function()
       vim.fn["mkdp#util#install"]()
     end,
+  },
+
+  {
+    "elixir-tools/elixir-tools.nvim",
+    version = "*",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local elixir = require("elixir")
+      local elixirls = require("elixir.elixirls")
+
+      elixir.setup({
+        nextls = { enable = true },
+        elixirls = {
+          enable = true,
+          settings = elixirls.settings({
+            dialyzerEnabled = false,
+            enableTestLenses = false,
+          }),
+          on_attach = function(client, bufnr)
+            vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+          end,
+        },
+        projectionist = {
+          enable = true,
+        },
+      })
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
   },
 }
