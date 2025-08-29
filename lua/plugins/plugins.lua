8a5721f98cb38367709a6892af91b502fbba0df6
Emil Privér <emil@priver.dev>
2025-08-29 14:52:21 +0200

    removed avante

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -406,53 +406,6 @@ return {
   },
 
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    provider = "gemini",
-    opts = {
-      provider = "gemini",
-      gemini = {
-        model = "gemini-2.5-pro",
-        timeout = 30000,
-        temperature = 1,
-        max_tokens = 4096,
-      },
-    },
-    build = "make",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
-
-  {
     "oysandvik94/curl.nvim",
     cmd = { "CurlOpen" },
     dependencies = {
