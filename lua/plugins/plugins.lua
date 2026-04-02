f1d39c82ec0feb9355f4c364091b072610c8a46b
Emil Privér <emil.priver@enad.io>
2026-04-02 08:52:32 +0200

    Swotch to fff

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -108,15 +108,26 @@ return {
     dependencies = { "hrsh7th/cmp-emoji" },
   },
 
-  -- add telescope-fzf-native
+  -- fff.nvim - fast file finder with frecency
   {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
+    "dmtrKovalenko/fff.nvim",
+    build = function()
+      require("fff.download").download_or_build_binary()
+    end,
+    lazy = false,
+    keys = {
+      { "ff", function() require('fff').find_files() end, desc = "FFFind files" },
+      { "fg", function() require('fff').live_grep() end, desc = "LiFFFe grep" },
+      {
+        "fz",
+        function() require('fff').live_grep({ grep = { modes = { "fuzzy", "plain" } } }) end,
+        desc = "Live fuzzy grep",
+      },
+      {
+        "fc",
+        function() require('fff').live_grep({ query = vim.fn.expand("<cword>") }) end,
+        desc = "Search current word",
+      },
     },
   },
 
