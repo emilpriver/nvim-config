c6bdcd4f0af06d9363cd1c9440f9734dac90dfd6
Emil Privér <emil.priver@enad.io>
2026-04-26 21:52:21 +0200

    Removed typescript.nvim

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -14,23 +14,8 @@ return {
         marksman = {},
         gleam = {},
       },
-      setup = {
-        tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
-          return true
-        end,
-      },
+      setup = {},
     },
-    dependencies = {
-      "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("snacks").util.lsp.on("attach", function(event)
-          local buffer = event.buf
-          -- stylua: ignore
-          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-        end)
-      end,
-    },
+    dependencies = {},
   },
 }
