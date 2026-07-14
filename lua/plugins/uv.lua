53d47815ab9bcf5c91a63fd0df96c2ad589e6486
Emil Privér <emil.priver@enad.io>
2026-07-14 13:38:15 +0200

    Added uv and gh

---
+++ lua/plugins/uv.lua
@@ -0,0 +1,14 @@
+return {
+  "benomahony/uv.nvim",
+  -- Optional filetype to lazy load when you open a python file
+  -- ft = { python }
+  -- Optional dependency, but recommended:
+  -- dependencies = {
+  --   "folke/snacks.nvim"
+  -- or
+  --   "nvim-telescope/telescope.nvim"
+  -- },
+  opts = {
+    picker_integration = true,
+  },
+}
